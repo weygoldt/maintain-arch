a79ac04a5d3635b320f06c97c35b5d5ebef57e64
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-22 15:04:20 +0100

    added update script

---
+++ pacman-tasks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# A collection of tasks to keep pacman up and running.
+
+# Update system
+echo $'\e[1;32mUpdating system ...\e[0m'
+sudo pacman -Syu
+paru -Syu
+
+# Delete non-installed cached packages
+echo $'\e[1;32mDeleting uninstalled packages from cache ...\e[0m'
+sudo pacman -Sc
+paru -Sc 
+
+# Clean unused dependencies
+echo $'\e[1;32mCleaning unused dependencies ...\e[0m'
+sudo pacman -Rsn $(pacman -Qdtq) 
+paru --clean
+
+# Refresh mirrors
+echo $'\e[1;32mRefreshing mirrors ...\e[0m'
+sudo reflector -c Germany -a 6 --sort rate --save /etc/pacman.d/mirrorlist
